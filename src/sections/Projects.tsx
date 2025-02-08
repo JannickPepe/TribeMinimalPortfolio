b2313494ff5f4e5f76702ad56c5ec14b5c5d650e
Jannick Pedersen <jannickpedersen@live.dk>
2025-02-08 21:52:05 +0100

    Implemented static project section

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -4,8 +4,9 @@ import image2 from "@/assets/images/project-2.jpg";
 import image3 from "@/assets/images/project-3.jpg";
 import image4 from "@/assets/images/project-4.jpg";
 import image5 from "@/assets/images/project-5.jpg";
+import Link from "next/link";
+import Image from "next/image";
 
-/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 const projects = [
   {
     name: "Artisan Brew Co.",
@@ -31,9 +32,34 @@ const projects = [
 
 const Projects: FC = () => {
   return (
-    <section>
+    <section className="py-24 md:py-28 lg:py-32">
       <div className="container">
-        Projects
+        <h2 className="text-4xl md:text-7xl lg:text-8xl">
+          Selected Works
+        </h2>
+
+        <div className="mt-10 md:mt-16 lg:mt-20">
+          {projects.map(({ name, image }) => (
+            <Link href={'/'} key={name} className="flex flex-col py-6 md:py-8 lg:py-10 border-t last:border-b border-stone-400 border-dotted">
+              <div className="">
+                <div className="aspect-video md:hidden">
+                  <Image src={image} alt={`${name} image`} className="size-full object-cover" />
+                </div>
+                <div className="mt-8 md:mt-0 flex justify-between items-center">
+                  <h3 className="text-2xl md:text-3xl lg:text-4xl">
+                    {name}
+                  </h3>
+                  <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+
       </div>
     </section>
   )
